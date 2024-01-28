9a040e8784c67de26a5dad17200348bda9266934
sikwan <yysskk99999@naver.com>
2024-01-28 21:03:05 +0900

    init

---
+++ RMQSoket/RMQSoketApp.swift
@@ -0,0 +1,17 @@
+//
+//  RMQSoketApp.swift
+//  RMQSoket
+//
+//  Created by 양시관 on 1/28/24.
+//
+
+import SwiftUI
+
+@main
+struct RMQSoketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
